a284ae6a8a5bd6c25a19993505f4cf9a524cb355
Maciej Łącki <maciej.lacky@gmail.com>
2021-06-05 17:31:28 +0200

    Add data models event and client class

--- XComAPI/Data/Client.cs
+++ XComAPI/Data/Client.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,16 @@ namespace XComAPI.Data
 {
     public class Client
     {
+        [Key]
+        public int IdClient { get; set; }
+        [Required(ErrorMessage = "Imie wymagane")]
+        [Display(Name = "Imie")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "E-mail wymagane")]
+        [Display(Name = "E-mail")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        public int IdEvent { get; set; }
+        public virtual Event Event { get; set; }
     }
 }
--- XComAPI/Data/Event.cs
+++ XComAPI/Data/Event.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,20 @@ namespace XComAPI.Data
 {
     public class Event
     {
+        [Key]
+        public int IdEvent { get; set; }
+        [Required(ErrorMessage = "Nazwa eventu wymagana")]
+        [Display(Name = "Nazwa eventu")]
+        public string EventName { get; set; }
+        [Display(Name = "Data od:")]
+        [DataType(DataType.Date)]
+        public DateTime DateFrom { get; set; }
+        [Display(Name = "Data do:")]
+        [DataType(DataType.Date)]
+        public DateTime DateTo { get; set; }
+        [Display(Name = "Opis eventu")]
+        public string EventDescription { get; set; }
+        [Display(Name = "Max ilość osób")]
+        public int MaxNumberOfPeople { get; set; }
     }
 }
