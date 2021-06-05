4a217fabc47fc3eef26b64b8a12522ae1644e0da
Maciej Łącki <maciej.lacky@gmail.com>
2021-06-05 17:37:12 +0200

    add event cotroller and client controller with enitity

---
+++ XComAPI/Data/XComAPIContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using XComAPI.Data;
+
+namespace XComAPI.Data
+{
+    public class XComAPIContext : DbContext
+    {
+        public XComAPIContext (DbContextOptions<XComAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<XComAPI.Data.Client> Client { get; set; }
+
+        public DbSet<XComAPI.Data.Event> Event { get; set; }
+    }
+}
