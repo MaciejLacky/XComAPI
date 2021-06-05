c5f1e2f1f08b5b3d931ad775a97f3177e689f7f7
Maciej Łącki <maciej.lacky@gmail.com>
2021-06-05 17:41:57 +0200

    add migrations

---
+++ XComAPI/Migrations/20210605153902_InitialCreate.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace XComAPI.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Event",
+                columns: table => new
+                {
+                    IdEvent = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EventName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateFrom = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateTo = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EventDescription = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MaxNumberOfPeople = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Event", x => x.IdEvent);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Client",
+                columns: table => new
+                {
+                    IdClient = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IdEvent = table.Column<int>(type: "int", nullable: false),
+                    EventIdEvent = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Client", x => x.IdClient);
+                    table.ForeignKey(
+                        name: "FK_Client_Event_EventIdEvent",
+                        column: x => x.EventIdEvent,
+                        principalTable: "Event",
+                        principalColumn: "IdEvent",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Client_EventIdEvent",
+                table: "Client",
+                column: "EventIdEvent");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Client");
+
+            migrationBuilder.DropTable(
+                name: "Event");
+        }
+    }
+}
